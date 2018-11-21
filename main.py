2143da970ad98f3082114843ced9b66bfb851c4e
Masao-Taketani <37681936+Masao-Taketani@users.noreply.github.com>
2018-11-21 20:05:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def decode(frame):
     print('Type: ', obj.type)
     print('Data: ', obj.data)
     font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(obj,'Detected QR codes:',(10, 500),font,4,(255,255,255),2,cv2.LINE_AA)
+    cv2.putText(obj,'Detected QR codes:',(10, 10),font,4,(255,255,255),2,cv2.LINE_AA)
   
   return decoded_objs
   
